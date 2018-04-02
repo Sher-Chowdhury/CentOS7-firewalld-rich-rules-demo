4af4e7484c31a05413ada910e92542caa31cb738
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-04-02 14:20:29 +0100

    wip

--- scripts/webserver-setup.sh
+++ scripts/webserver-setup.sh
@@ -3,18 +3,14 @@
 set -ex
 
 echo '##########################################################################'
-echo '################## About to run source-rpms.sh script ####################'
+echo '################## About to run webserver-rpms.sh script ####################'
 echo '##########################################################################'
 
 
-echo '10.0.0.0/24 via 192.168.10.100' >> /etc/sysconfig/network-scripts/route-enp0s8
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
 
-systemctl restart network
+
 
 exit 0
-
-
-# perform tests with:
-# ping -c 3 10.0.0.11
-# ssh 10.0.0.11
-# traceroute 10.0.0.11
