aacc4f9e61682bd428184c750de086ade684f43c
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-08-20 11:13:06 +0100

    wip

---
+++ hello.rb
@@ -0,0 +1,5 @@
+required_plugins = ['vagrant-hosts', 'vagrant-share', 'vagrant-vbox-snapshot', 'vagrant-host-shell', 'vagrant-reload']
+for required_plugins.each do |pluginss|
+#  puts "#{plugin}"
+   puts "hello"
+end
